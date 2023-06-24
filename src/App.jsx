0cdf0891ea9617e2b1aa9f2226efde31231bc9d0
Asaduzzaman Sunam <asaduzzaman.sunam@gmail.com>
2023-06-24 11:09:22 +0600

    navbar max width changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Navbar from "./Pages/Shared/Navbar/Navbar";
-import Main from "../src/Layout/Main"
+import Main from "../src/Layout/Main";
 import Footer from "./Pages/Shared/Footer/Footer";
 
 function App() {
-  return <>
-  <div className="max-w-[1440px] mx-auto">
-      <Navbar/>
-      <Main/>
-      <Footer/>
-  </div>
-
-  </>;
+  return (
+    <>
+      <div className="max-w-screen-2xl mx-auto">
+        <Navbar />
+        <Main />
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default App;
