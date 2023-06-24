2827ed97bf9a34025f423499bfa5b4f06cdd826d
Asaduzzaman Sunam <asaduzzaman.sunam@gmail.com>
2023-06-24 12:36:50 +0600

    color  information updated on Readme

--- Readme.md
+++ Readme.md
@@ -10,3 +10,23 @@ In the project directory, you can run:
 
 Runs the app in the development mode.\
 Open [http://127.0.0.1:5173/](http://127.0.0.1:5173) to view it in your browser.
+
+## Design Decisions:
+
+Tailwind CSS & Daisy UI has been used in this project.
+
+### Color
+
+This project uses Tailwind Colors. Every Color has 9 shades from 100-900.\
+ex: bg-green-100, text-green-900
+
+#### Brand Color:
+
+- `green`
+
+#### Supporting Colors:
+
+- confirmation: `green`
+- info: `teal`
+- warning: `orange`
+- danger: `red`
--- src/Pages/Home/Homepage.jsx
+++ src/Pages/Home/Homepage.jsx
@@ -6,16 +6,6 @@ const Homepage = () => {
   return (
     <div>
       <Headline>This is homepage</Headline>
-      <Subtitle>This is a supporting text</Subtitle>
-      <Paragraph>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ipsa
-        odio beatae totam sapiente facere molestias debitis, nemo illo
-        molestiae? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Architecto error veniam ipsa similique, illum minima sint consequuntur
-        natus rerum, odit distinctio sunt omnis expedita eum voluptates
-        obcaecati tenetur facilis. Asperiores earum, ab dicta vitae laborum
-        laudantium incidunt commodi fugit voluptates.
-      </Paragraph>
     </div>
   );
 };
