5dbfdbce8702e6d490c182cadfcab031d8c144a8
sunam <asaduzzaman.sunam@gmail.com>
2023-06-27 00:40:55 +0600

    doctors assets created and doctors mock data created

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Appointment from "../Pages/Appointment/Appointment/Appointment";
 
 import Homepage from "../Pages/Home/Home/Homepage";
 import NotFound from "../Pages/NotFound/NotFound";
@@ -7,6 +9,7 @@ const Main = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/appointment" element={<Appointment />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
